140dcefcf9ea1b1df797499225bc124d290eeff3
qira ahmad <qira.ahmad@gmail.com>
2021-04-20 06:34:21 +0500

    initial landing page and sign in

---
+++ app.py
@@ -0,0 +1,30 @@
+
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, render_template
+  
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
+  
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/campus_career_portal')
+def main():
+    return render_template("admin_signin.html")
+
+@app.route('/learn_more')
+def learn_more():
+    return render_template("learn_more.html")
+  
+# main driver function
+if __name__ == '__main__':
+  
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
